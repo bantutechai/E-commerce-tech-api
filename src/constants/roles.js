6a9a9a7d740d13ba62aea925e11b04b0a4432292
BantuTech AI <bantutechai@gmail.com>
2026-08-21 23:05:46 +0100

    Add roles and export constants in roles.js

---
+++ src/constants/roles.js
@@ -0,0 +1,13 @@
+const ROLES = {
+  SUPER_ADMIN: 'SUPER_ADMIN',
+  ADMIN: 'ADMIN',
+  EDITOR: 'EDITOR',
+  INVENTORY_MANAGER: 'INVENTORY_MANAGER',
+};
+
+const ALL_ROLES = Object.values(ROLES);
+
+module.exports = {
+  ROLES,
+  ALL_ROLES,
+};
